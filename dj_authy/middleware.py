cdc375b4d9f0d7973f88f7166524b4b0ca490ad7
nickdelja <nickdelja@gmail.com>
2016-07-26 11:15:08 -0700

    Forcing authentication

--- dj_authy/middleware.py
+++ dj_authy/middleware.py
@@ -22,27 +22,17 @@ class AuthyAuthenticationRequiredMiddleware(object):
     """
     def process_request(self, request):
         if request.user.is_authenticated():
-            #
-            # only if we dont already have it
-            #
-            if request.session.get(AUTHY_SESSION_KEY, None) in [None, False]:
+            # then use wants authy auth
+            if not request.session.get(AUTHY_SESSION_KEY):
 
-                profile_data = request.user.authy_profile.data
+                # but has not authenticated with authy yet
+                if request.path not in [reverse_lazy('dj_authy:holding'),
+                                        reverse_lazy('dj_authy:profile'),
+                                        settings.LOGOUT_URL]:
+                    # and they are not looking at the holding or profile setup page
+                    logger.info(u'User authenticating with authy' % request.user)
 
-                # if the required key is present in the profile data and is set to True
-                if AUTHY_IS_REQUIRED_KEY in profile_data and profile_data[AUTHY_IS_REQUIRED_KEY] is True:
+                    url = _url_to_appropriate_authy_page(request, AUTHY_SESSION_KEY)
 
-                    # then use wants authy auth
-                    if not request.session.get(AUTHY_SESSION_KEY):
-
-                        # but has not authenticated with authy yet
-                        if request.path not in [reverse_lazy('dj_authy:holding'),
-                                                reverse_lazy('dj_authy:profile'),
-                                                settings.LOGOUT_URL]:
-                            # and they are not looking at the holding or profile setup page
-                            logger.info(u'User authenticating with authy' % request.user)
-
-                            url = _url_to_appropriate_authy_page(request, AUTHY_SESSION_KEY)
-
-                            return redirect(url)
+                    return redirect(url)
         return None
