f8c29e53bba8b619abefd4f53716f6962dc96c2a
nickdelja <nickdelja@gmail.com>
2016-08-17 16:48:10 -0700

    Update forms.py

--- dj_authy/forms.py
+++ dj_authy/forms.py
@@ -10,7 +10,7 @@ class BaseAuthyMediaForm(forms.ModelForm):
     """
     class Media:
         css = {
-            css = ('//cdnjs.cloudflare.com/ajax/libs/authy-form-helpers/2.3/form.authy.min.css',)
+            'all': ('//cdnjs.cloudflare.com/ajax/libs/authy-form-helpers/2.3/form.authy.min.css',)
         }
         js = ('//cdnjs.cloudflare.com/ajax/libs/authy-form-helpers/2.3/form.authy.min.js',)
 
