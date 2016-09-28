7fe5f4275fd22298a966be1b72f225500af28f69
nickdelja <nickdelja@gmail.com>
2016-09-28 09:54:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="django-authy",
     packages=['dj_authy'],
-    version='0.1.0',
+    version='0.1.1',
     author="Ross Crawford-d'Heureuse",
     license="MIT",
     author_email="ross@lawpal.com",
